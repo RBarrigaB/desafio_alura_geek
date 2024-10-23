2c3f32bbffe9312af0425b69fcc75451b1868538
Reinaldo Barriga <rbarriga.ing@gmail.com>
2024-10-23 00:43:44 -0300

    Se agregan cambios en html, configuraciones en css y nuevas etiquetas en el json de configuracion aplicadas mediante onInit

--- js/onInit.js
+++ js/onInit.js
@@ -3,13 +3,21 @@ window.onload = function () {
   let titleHeader = document.getElementById("title-header");
   let misProductosTitulo = document.getElementById("misProductosTitulo");
   let agregarProductoTitulo = document.getElementById("agregarProductoTitulo");
+  let nombreForm = document.getElementById("nombreForm");
+  let priceForm = document.getElementById("priceForm");
+  let imagenForm = document.getElementById("imagenForm");
+  let copyright = document.getElementById("footer__copyright");
 
   fetch("../labels.json")
     .then((response) => response.json()) // Convierte la respuesta en formato JSON
     .then((data) => {
-      titleHeader.innerHTML = data.titleHeader;
-      misProductosTitulo.innerHTML = data.agregarProductoTitulo;
-      agregarProductoTitulo.innerHTML = data.misProductosTitulo;  
+      titleHeader.innerHTML = data.titleHeader + '&#128311; &#128311; &#128311;';
+      misProductosTitulo.innerHTML = data.misProductosTitulo;
+      agregarProductoTitulo.innerHTML = data.agregarProductoTitulo;  
+      nombreForm.placeholder = data.nombreFormPlaceholder;
+      priceForm.placeholder = data.precioFormPlaceholder;
+      imagenForm.placeholder = data.imagenFormPlaceholder;
+      copyright.innerHTML = data.titleHeader + '&#128311; &#128311; &#128311; <br>' + data.copyrightFooter;
     })
     .catch((error) => {
       alert.error("Error al cargar los datos:", error);
